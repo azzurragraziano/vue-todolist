eb1a6f2d7e6d182f078bb5e8abb8c486f4209aa1
azzurragraziano <azzurragraziano02@email.com>
2022-06-27 17:36:23 +0200

    milestone 1,2,3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,40 @@
 var app = new Vue(
     {
         el:'#app',
+        data: {
+            newToDo:'',
+            toDoList: [
+                {
+                    text: 'fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'fare il bucato',
+                    done: false
+                }
+            ]
+        },
+        methods: {
+            addNewToDo() {
+
+                if(this.newToDo.length > 0) {
+                    // Pushare nell'array newToDo
+                    this.toDoList.push(
+                        {
+                            text: this.newToDo,
+                            done: false
+                        }
+                    );
+                    this.newToDo = '';
+                }
+            },
+            removeTodo(index) {
+                this.toDoList.splice(index, 1)
+            }
+        }
     }
 )
